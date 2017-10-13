fc01cbec29bbffc41a9d64c1040c6b0102dbc457
Adryd <Adryd@users.noreply.github.com>
2017-10-13 18:33:10 -0400

    fixed some things

--- index.js
+++ index.js
@@ -2,55 +2,55 @@ const express = require('express');
 const config = require('./config.js');
 const fetch = require('snekfetch');
 const btoa = require('btoa');
-const db = require('mongodb').MongoClient;
-const assert = require('assert');
+const path = require('path');
 
-db.connect(config.dbUrl, function(err, db) {
-  assert.equal(null, err);
-  console.info("Connected to db");
+const app = express();
 
-  const app = express();
-
-  app.get('/', function(req, res) {
-    console.log();
-    res.send('<a href="/auth">Log in with Discord</a>');
-  });
-
-  let redirectUri = encodeURIComponent(`${config.webappUri}/auth/callback`)
-  app.get('/auth', function(req, res) {
-    res.redirect(`https://discordapp.com/oauth2/authorize?client_id=${config.discordClientID}&scope=${config.apiScope}&response_type=code&redirect_uri=${redirectUri}`);
-  });
-  app.get('/auth/callback', async (req, res) => {
-    if (!req.query.code) throw new Error('NoCodeProvided');
-    const code = req.query.code;
-    const creds = btoa(`${config.discordClientID}:${config.discordClientSecret}`);
-    const auth = await fetch.post(`https://discordapp.com/api/oauth2/token?grant_type=authorization_code&code=${code}&redirect_uri=${redirectUri}`, {headers: {
-      Authorization: `Basic ${creds}`,
-    }})
-    const jsonAuth = JSON.parse(auth.text)
-    if (jsonAuth.scope !== "identify email guilds guilds.join") {
-      res.send('bad scope')
-      return;
-    }
-    const user = await fetch.get(`https://discordapp.com/api/users/@me`, {headers: {
-      Authorization: `Bearer ${jsonAuth.access_token}`,
-    }})
-    const jsonUser = JSON.parse(user.text)
-    var userst = {}
-    userst[jsonUser.id] = {
-      refresh_token: jsonAuth.refresh_token,
-      username: jsonUser.username,
-      discriminator: jsonUser.discriminator,
-      email: jsonUser.email,
-      avatar: jsonUser.avatar
-    }
-    insertDocuments(db,userst)
-  });
-
-  app.listen(config.webappPort)
+let redirectUri = encodeURIComponent(`${config.webappUri}/auth/callback`)
+app.get('/', function(req, res) {
+  res.redirect(`https://discordapp.com/oauth2/authorize?client_id=${config.discordClientID}&scope=${config.apiScope}&response_type=code&redirect_uri=${redirectUri}`);
 });
 
-var insertDocuments = function(db, data) {
-  var collection = db.collection('users');
-  collection.insert(data);
-}
+app.get('/auth/callback', async(req, res) => {
+  if (!req.query.code) throw new Error('NoCodeProvided');
+  const code = req.query.code;
+  const creds = btoa(`${config.discordClientID}:${config.discordClientSecret}`);
+  const authReq = await fetch.post(`https://discordapp.com/api/oauth2/token?grant_type=authorization_code&code=${code}&redirect_uri=${redirectUri}`, {
+    headers: {
+      Authorization: `Basic ${creds}`,
+    }
+  })
+  const auth = JSON.parse(authReq.text);
+  const userReq = await fetch.post(`https://discordapp.com/api/oauth2/token?grant_type=authorization_code&code=${code}&redirect_uri=${redirectUri}`, {
+    headers: {
+      Authorization: `Bearer ${auth.authorization_code}`,
+    }
+  })
+  const user = JSON.parse(userReq.text);
+  // At this point we store an object in a database similar to the one below
+  // This will allow us to invite them at any point, perhaps inviting multiple users in waves after being approved
+  // Another idea I had was to allow for applications to contain a comment or something so it can be used to replace the memework #recruits channel
+  const thingtowritetodb = {
+    12345: { // replace 12345 with user id
+      refresh_token: auth.refresh_token,
+      username: user.username,
+      discrim: user.discriminator,
+      avatar: user.avatar, // add condition if user.avatar is undefined
+      email: user.email // same here
+    }
+  }
+  req.redirect(`/submit?token=${auth.access_token}`)
+});
+
+app.get('/submit', async(req, res) => {
+  if (!req.query.token) throw new Error('NoCodeProvided');
+  const token = req.query.token;
+  const invite = await fetch.post(`https://discordapp.com/api/invites/G76FYaR`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    }
+  })
+  res.send('ok')
+});
+
+app.listen(config.webappPort)
